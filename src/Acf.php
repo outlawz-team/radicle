4cd187176cabd8afd2cec2d03f34693788aa46fe
Jur Dekker <jur@outlawz.nl>
2024-03-21 09:28:27 +0100

    Add options pages to config

--- src/Acf.php
+++ src/Acf.php
@@ -31,6 +31,21 @@ class Acf
      */
     public function boot()
     {
+        if(function_exists('acf_add_options_page') ) {
+            foreach (config('acf.options_pages', []) as $options_page) {
+                $parent_slug = $options_page['menu_slug'];
+                acf_add_options_page($options_page);
+
+                if (function_exists('acf_add_options_sub_page')) {
+                    if (isset($options_page['sub_pages'])) {
+                        foreach ($options_page['sub_pages'] as $sub_page) {
+                            acf_add_options_sub_page(['parent_slug' => $parent_slug, ...$sub_page]);
+                        }
+                    }
+                }
+            }
+        }
+
         if(function_exists('acf_add_local_field_group')) {
             $acfFiles = $this->getAllAcfClasses();
             foreach ($acfFiles as $acfFile) {
--- src/Providers/AcfServiceProvider.php
+++ src/Providers/AcfServiceProvider.php
@@ -19,6 +19,11 @@ class AcfServiceProvider extends ServiceProvider
         $this->app->singleton('Acf', function () {
             return new Acf($this->app);
         });
+
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/acf.php',
+            'acf'
+        );
     }
 
     /**
@@ -28,6 +33,10 @@ class AcfServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/../../config/acf.php' => $this->app->configPath('acf.php'),
+        ], 'acf');
+
         if(function_exists('acf_add_local_field_group')){
             $this->commands([
                 MakeAcfCommand::class,
