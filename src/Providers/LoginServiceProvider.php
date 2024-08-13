778b1e1501178a7995d948d8cd3c4c900c4d3a5b
outlawz-bas <bas@outlawz.nl>
2024-08-13 15:35:30 +0200

    fix: styled show/hide password

--- src/Providers/LoginServiceProvider.php
+++ src/Providers/LoginServiceProvider.php
@@ -112,6 +112,14 @@ class LoginServiceProvider extends ServiceProvider
                 content: url(data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%2020%2020%27%3E%3Cpath%20d%3D%27M14.83%204.89l1.34.94-5.81%208.38H9.02L5.78%209.67l1.34-1.25%202.57%202.4z%27%20fill%3D%27%23FFB800%27%2F%3E%3C%2Fsvg%3E);
             }
 
+            /* Show hide Password icon*/
+            .login .button.wp-hide-pw:focus {
+                background: transparent;
+                border-color: #FFB800;
+                box-shadow: 0 0 0 1px #FFB800;
+                outline: 2px solid transparent;
+            }
+                
             /* Box */
             .login form{
                 padding: 40px;
